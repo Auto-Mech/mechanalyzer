8a2286f11b760e56d2aef44cfe462831eb9eaabf
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2020-02-11 16:44:54 -0600

    update

--- ratefit/__init__.py
+++ ratefit/__init__.py
@@ -6,21 +6,11 @@ Module deal with rate constant functional forms; Either
      fitting parameters are known
 """
 
-from ratefit.fxns import single_arrhenius
-from ratefit.fxns import double_arrhenius
-from ratefit.fxns import lindemann
-from ratefit.fxns import troe
-from ratefit.err import assess_pressure_dependence
-from ratefit.err import calc_sse_and_mae
+from ratefit import calc
 from ratefit import fit
 
 
 __all__ = [
-    'single_arrhenius',
-    'double_arrhenius',
-    'lindemann',
-    'troe',
-    'assess_pressure_dependence',
-    'calc_sse_and_mae',
+    'calc',
     'fit'
 ]
