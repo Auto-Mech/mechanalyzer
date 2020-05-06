f6fcbe489e812e644709b8fe19b4f5d616404966
Kevin Moore <kmoore@blueslogin3.lcrc.anl.gov>
2020-05-06 02:47:00 -0500

    update tests

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@
 from distutils.core import setup
 
 setup(name="RateFit",
-      version="0.1.1",
+      version="0.1.0",
       packages=['ratefit',
                 'ratefit.calc',
                 'ratefit.fit',
                 'ratefit.fit.arrhenius',
-                'ratefit.fit.troe']),
-      package_dir={},
-      package_data={}
+                'ratefit.fit.troe',
+                'transformations']),
+      package_dir={'ratefit': 'ratefit'
+                   'transformations': 'transformations'},
+      package_data={'ratefit': ['tests/data/*.txt', 'tests/data/*.csv']})
       ])
---
+++ troefit/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p build
+cd build
+
+cmake . -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_Fortran_COMPILER=$FC -DCMAKE_Fortran_FLAGS="${FFLAGS}"
+
+make VERBOSE=1
+make install
