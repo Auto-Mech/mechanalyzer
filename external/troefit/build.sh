eefe1bb8232041c0f9e48d3edf956b1098482d21
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2020-05-06 22:14:57 -0500

    update

--- external/troefit/build.sh
+++ external/troefit/build.sh
@@ -3,7 +3,7 @@
 mkdir -p build
 cd build
 
-cmake . -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_Fortran_COMPILER=$FC -DCMAKE_Fortran_FLAGS="${FFLAGS}"
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_Fortran_COMPILER=$FC -DCMAKE_Fortran_FLAGS="${FFLAGS}"
 
 make VERBOSE=1
 make install
