4f3119872b02c4c7ea4fb6f1662cd9115de2ffef
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2020-06-07 00:16:55 -0500

    add lowp calc and fix dens calc

--- ratefit/calc/__init__.py
+++ ratefit/calc/__init__.py
@@ -5,10 +5,12 @@ Calculates k(T, P) and using various functional forms
 from ratefit.calc.rates import single_arrhenius
 from ratefit.calc.rates import double_arrhenius
 from ratefit.calc.rates import arrhenius
+from ratefit.calc.rates import lowp_limit
 from ratefit.calc.rates import lindemann
 from ratefit.calc.rates import troe
 from ratefit.calc.rates import plog
 from ratefit.calc.rates import chebyshev
+from ratefit.calc.rates import p_to_m
 from ratefit.calc.pdep import assess_pressure_dependence
 from ratefit.calc.err import fitting_errors
 
@@ -17,10 +19,12 @@ __all__ = [
     'single_arrhenius',
     'double_arrhenius',
     'arrhenius',
+    'lowp_limit',
     'lindemann',
     'troe',
     'plog',
     'chebyshev',
+    'p_to_m',
     'assess_pressure_dependence',
     'fitting_errors',
 ]
