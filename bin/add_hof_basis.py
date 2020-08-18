ef302edcaf062aff9d193ef335b07afd01a758b8
snelliott <sarah.elliott26@uga.edu>
2020-08-18 14:34:30 -0500

    script in a main function

--- bin/add_hof_basis.py
+++ bin/add_hof_basis.py
@@ -10,6 +10,10 @@ import time
 CWD = os.getcwd()
 INNAME = sys.argv[1]
 OUTNAME = sys.argv[2]
+PARALLEL = False
+if len(sys.argv) > 3:
+    if 'p' in sys.argv[3].lower() :
+        PARALLEL = True    
 
 t0 = time.time()
 # Read input species file
@@ -18,7 +22,7 @@ with open(os.path.join(CWD, INNAME), 'r') as file_obj:
 
 # Write new string
 mechanalyzer.parser.spc.write_basis_csv(
-    SPC_STR, outname=OUTNAME, path=CWD)
+    SPC_STR, outname=OUTNAME, path=CWD, parallel=PARALLEL)
 tf = time.time()
 
 print('time to complete: {:.2f}'.format(tf-t0))
