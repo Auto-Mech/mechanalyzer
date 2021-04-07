916fd196c850c6b18ebdcf31f52250d267372986
sjklipp <sjk@anl.gov>
2021-04-07 17:02:33 -0500

    rate threshold check revision

--- ratefit/ratefit/fit/util.py
+++ ratefit/ratefit/fit/util.py
@@ -76,14 +76,27 @@ def get_valid_tk(temps, rate_constants, bimol,
     # Grab the temperature, rate constant pairs which correspond to
     # temp > 0, temp within tmin and tmax, rate constant defined (not ***)
     valid_t, valid_k = [], []
-    if not np.isclose(tmin, tmax):
-        for temp, rate_constant in zip(temps, rate_constants):
+    # Grab the temperature, rate constant pairs which correspond to
+    # temp > 0, temp within tmin and tmax, rate constant defined (not ***)
+    valid_t, valid_k = [], []
+    for temp, rate_constant in zip(temps, rate_constants):
+        if rate_constant == '***':
+            continue
+        kthresh = 0.0 if not bimol else 1.0e-24
+        if float(rate_constant) > kthresh and tmin <= temp <= tmax:
+            valid_t.append(temp)
+            valid_k.append(rate_constant)
 
     # Convert the lists to numpy arrays
     valid_t = np.array(valid_t, dtype=np.float64)
     valid_k = np.array(valid_k, dtype=np.float64)
 
+    # if not np.isclose(tmin, tmax):
+    #     for temp, rate_constant in zip(temps, rate_constants):
+
     return valid_t, valid_k
+
+
 
 
 def flip_ktp_dct(ktp_dct):
