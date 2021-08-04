0d9dbeee7eaafb33c9824883faf01089da074378
Kevin Moore <kmoore@blueslogin3.lcrc.anl.gov>
2021-08-03 23:00:50 -0500

    clean and fix prepare_basis and unirefs in thermfit

--- thermfit/_basis.py
+++ thermfit/_basis.py
@@ -1,5 +1,8 @@
-"""
-  Therm Calculations
+""" ThermFit Interface to CBH code used to generate and update
+    the mechanism oriented objects with species required to
+    form the complete basis of heats-of-formation for species
+    currently comprising some mechanism and stored in the
+    the species dictionary.
 """
 
 import os
@@ -20,6 +23,9 @@ def prepare_basis(ref_scheme, spc_dct, spc_names,
         Executed in parallel using Python MultiProcessing
         module if nprocs set above 1. Parallelization occurs
         over the species list.
+
+        species basis_dct[name] = ()
+        ts basis_dct[name] = ()
     """
 
     args = (ref_scheme, spc_dct, zrxn, print_log)
@@ -61,19 +67,30 @@ def _prepare_basis(ref_scheme, spc_dct, zrxn, print_log,
             radrad = automol.reac.is_radical_radical(zrxn)
             if (rcls, radrad) in thermfit.cbh.CBH_TS_CLASSES:
                 scheme = ref_scheme
+                if '_' in scheme:
+                    scheme = 'cbh' + scheme.split('_')[1]
             else:
                 scheme = 'basic'
-            if '_' in scheme:
-                scheme = 'cbh' + scheme.split('_')[1]
             spc_basis, coeff_basis = thermfit.cbh.ts_basis(
                 zrxn, scheme)
         else:
             spc_basis, coeff_basis = thermfit.cbh.species_basis(
                 spc_ich, ref_scheme)
-        spc_basis = tuple(automol.inchi.add_stereo(bas) for bas in spc_basis
-                          if isinstance(bas, str))
 
-        msg += '\nInChIs for basis set: {}'.format('\n  '.join(spc_basis))
+        # Add stereochemistry to the basis
+        # basis either single InChI, or ((InChI,), (InChI,))
+        ste_basis = ()
+        for bas in spc_basis:
+            if isinstance(bas, str):
+                ste_basis += (automol.inchi.add_stereo(bas),)
+            else:
+                ste_basis += (
+                    (tuple(automol.inchi.add_stereo(b) for b in bas[0]),
+                     tuple(automol.inchi.add_stereo(b) for b in bas[1])),
+                )
+
+        # need to figure out print for TS basis
+        # msg += '\nInChIs for basis set: {}'.format('\n  '.join(ste_basis))
 
         # Add to the dct containing info on the species basis
         basis_dct[spc_name] = (spc_basis, coeff_basis)
@@ -86,58 +103,65 @@ def _prepare_basis(ref_scheme, spc_dct, zrxn, print_log,
     output_queue.put((basis_dct,))
 
 
-def unique_references(basis_dct, spc_dct):
-    """ Take one or more basis dictionaries and obtain the uniqueness of them
+# Build a dictionary of unique basis references that need to be added
+def unique_basis_species(basis_dct, spc_dct):
+    """ Checks to see which species comprising the lists of
+        heat-of-formation basis species (provided in the basis dct)
+        currently exist in the mechanism species dictionary.
+
+        If a basis molecule is found to be absent a sub-species
+        dictionary is created and added to the spc dictionary.
+
+        :param basis_dct: basis molecules for a set of species
+        :type basis dct: dict[str: tuple(str)]
     """
 
-    def _spc_ref_unique(ref, dct_ichs, bas_ichs):
+    def _spc_ref_unique(ref, mech_ichs):
         """ Determine if InChI is unique
         """
-        return all(
-            ref not in lst
-            for lst in (spc_ichs, dct_ichs, bas_ichs))
+        return ref not in mech_ichs
 
-    def _ts_ref_unique(ref, dct_ichs, bas_ichs):
+    def _ts_ref_unique(ref, mech_ichs):
         """ Determine if lst of inchis for ts is unique """
         return (
-            _ich_unique(ref, dct_ichs, bas_ichs) or
-            _ich_unique(ref[::-1], dct_ichs, bas_ichs[::-1])
+            _spc_ref_unique(ref, mech_ichs) or
+            _spc_ref_unique(ref[::-1], mech_ichs)
         )
 
     # Generate list of all species currently in the spc dct
-    dct_ichs = [spc_dct[spc]['inchi'] for spc in spc_dct.keys()
-                if 'ts' not in spc]
+    mech_ichs = tuple(spc_dct[spc]['inchi'] for spc in spc_dct.keys()
+                      if 'ts' not in spc)
 
     # Generate list of all prospective basis species
-    bas_ichs = ()
+    unique_refs_dct = {}
     cnt = 1
-    for ref_ich, bas_ichs in basis_dct.items():
-        if isinstance(bas, str):
-            # Generate info for the species 
-            if _spc_ref_unique(ref, dct_ichs, bas_ichs):
-                ref_name = 'REF_{}'.format(cnt)
-                unique_refs_dct[ref_name] = create_spec(ref)
-                bas_ichs += ichs
-                cnt +=1
-        else:
-            # Generate info for TS if unique
-            if _ts_ref_unique(ref, dct_ichs, bas_ichs):
-                ref_name = 'TS_REF_{}'.format(cnt)
-                unique_refs_dct[ref_name] = create_ts_spc(
-                    ref, spc_dct, spc_dct[spc_name]['mult'],
-                    rcls)
-                bas_ichs += ichs
-                cnt +=1
+    for name, (basis, _) in basis_dct.items():
+        for bas in basis:
+            # bas spc is (1) string = species, (2) ((str,), (str,))
+            if isinstance(bas, str):
+                if _spc_ref_unique(bas, mech_ichs):
+                    ref_name = 'REF_{}'.format(cnt)
+                    unique_refs_dct[ref_name] = create_spec(bas)
+                    mech_ichs += (bas,)
+                    cnt += 1
+            else:
+                if _ts_ref_unique(bas, mech_ichs):
+                    ref_name = 'TS_REF_{}'.format(cnt)
+                    unique_refs_dct[ref_name] = create_ts_spc(
+                        bas, spc_dct, spc_dct[name]['mult'])
+                    mech_ichs += (bas,)
+                    cnt += 1
 
     return unique_refs_dct
 
 
 # Create dictionries of information for species/TS
-def create_ts_spc(ref, spc_dct, mult, rxnclass):
+def create_ts_spc(ref, spc_dct, mult):
     """ add a ts species to the species dictionary
     """
 
     # Obtain the Reaction InChIs, Charges, Mults
+    print('ref test', ref)
     reacs, prods = ref[0], ref[1]
     rxn_ichs = (
         tuple(automol.inchi.add_stereo(ich) for ich in reacs if ich),
@@ -168,7 +192,6 @@ def create_ts_spc(ref, spc_dct, mult, rxnclass):
         'prods': list(prods),
         'charge': 0,
         'inchi': '',
-        'class': rxnclass,
         'mult': mult,
         'ts_locs': (0,),
         'rxn_info': rinfo.from_data(rxn_ichs, rxn_chgs, rxn_muls, mult)
--- thermfit/tests/test__basis.py
+++ thermfit/tests/test__basis.py
@@ -12,10 +12,10 @@ SPC_DCT = {
         'inchi': 'InChI=1S/C2H6/c1-2/h1-2H3',
         'mult': 1,
         'charge': 0},
-    'C2H5': {
-        'inchi': 'InChI=1S/C2H5/c1-2/h1H2,2H3',
-        'mult': 2,
-        'charge': 0},
+    # 'C2H5': {
+    #     'inchi': 'InChI=1S/C2H5/c1-2/h1H2,2H3',
+    #     'mult': 2,
+    #     'charge': 0},
     'H': {
         'inchi': 'InChI=1S/H',
         'mult': 2,
@@ -160,10 +160,6 @@ products keys:
 HABS_ZRXN = automol.reac.from_string(HABS_ZRXN_STR)
 
 
-# Scheme information
-SCHEME = 'basic'
-
-
 def test__species():
     """ test thermfit.._basis.prepare_basis
     """
@@ -176,7 +172,7 @@ def test__species():
     }
 
     dct1 = thermfit.prepare_basis(
-        SCHEME, SPC_DCT, SPC_NAMES,
+        'basic', SPC_DCT, SPC_NAMES,
         nprocs='auto', print_log=False, zrxn=None)
 
     _check_dct(ref_dct1, dct1)
@@ -188,34 +184,44 @@ def test__transition_state():
 
     ref_dct1 = {
         'ts_1_1_1': (
-            ('InChI=1S/H2/h1H', 'InChI=1S/CH4/h1H4',
-             'InChI=1S/H2O/h1H2'),
-            numpy.array([-7.5,  7.0,  2.0]))
+            ('InChI=1S/H2/h1H', 'InChI=1S/CH4/h1H4', 'InChI=1S/H2O/h1H2'),
+            numpy.array([-7.5,  7.0,  2.0])
+        )
     }
     ref_dct2 = {
         'ts_1_1_1': (
-            ('InChI=1S/CH4/h1H4', 'InChI=1S/H2O/h1H2',
-             'InChI=1S/H2/h1H'),
-            [6.0, 1.0, 1.0, -7.0])
+            ['InChI=1S/CH4/h1H4', 'InChI=1S/H2O/h1H2',
+             (('InChI=1S/CH3/h1H3', 'InChI=1S/H2O/h1H2'),
+              ('InChI=1S/CH4/h1H4', 'InChI=1S/HO/h1H')),
+             'InChI=1S/H2/h1H'],
+            [6.0, 1.0, 1.0, -7.0]
+        )
     }
     ref_dct3 = {
         'ts_1_1_1': (
-            ('InChI=1S/CH4O/c1-2/h2H,1H3',
-             'InChI=1S/C2H6/c1-2/h1-2H3',
-             'InChI=1S/CH4/h1H4',
-             'InChI=1S/H2O/h1H2'),
-            [1.0, 3.0, 1.0, 2.0, -3, -1, -2])
+            ['InChI=1S/CH4O/c1-2/h2H,1H3', 'InChI=1S/C2H6/c1-2/h1-2H3',
+             (('InChI=1S/CH3/h1H3', 'InChI=1S/H2O2/c1-2/h1-2H'),
+              ('InChI=1S/CH4/h1H4', 'InChI=1S/HO2/c1-2/h1H')),
+             (('InChI=1S/C2H5/c1-2/h1H2,2H3', 'InChI=1S/H2O/h1H2'),
+              ('InChI=1S/C2H6/c1-2/h1-2H3', 'InChI=1S/HO/h1H')),
+             'InChI=1S/CH4/h1H4', 'InChI=1S/H2O/h1H2',
+             (('InChI=1S/CH3/h1H3', 'InChI=1S/H2O/h1H2'),
+              ('InChI=1S/CH4/h1H4', 'InChI=1S/HO/h1H'))],
+            [1.0, 3.0, 1.0, 2.0, -3, -1, -2]
+        )
     }
     ref_dct4 = {
         'ts_1_1_1': (
-            ('InChI=1S/CH4/h1H4',
-             'InChI=1S/H2O/h1H2',
-             'InChI=1S/H2/h1H'),
-            [6.0, 1.0, 1.0, -7.0])
+            ['InChI=1S/CH4/h1H4', 'InChI=1S/H2O/h1H2',
+             (('InChI=1S/CH3/h1H3', 'InChI=1S/H2O/h1H2'),
+              ('InChI=1S/CH4/h1H4', 'InChI=1S/HO/h1H')),
+             'InChI=1S/H2/h1H'],
+            [6.0, 1.0, 1.0, -7.0]
+        )
     }
 
     dct1 = thermfit.prepare_basis(
-        SCHEME, SPC_DCT, TS_NAMES,
+        'basic', SPC_DCT, TS_NAMES,
         nprocs='auto', print_log=False, zrxn=HABS_ZRXN)
     dct2 = thermfit.prepare_basis(
         'cbh0', SPC_DCT, TS_NAMES,
@@ -244,9 +250,73 @@ def _check_dct(ref_dct, dct):
 def test__unique():
     """ test thermfit._basis.
     """
-    pass
+
+    ref_uni_dct = {
+        'REF_1': {
+            'smiles': 'CO',
+            'inchi': 'InChI=1S/CH4O/c1-2/h2H,1H3',
+            'inchikey': 'OKKJLVBELUTLKV-UHFFFAOYSA-N',
+            'charge': 0, 'mult': 1,
+            'mc_nsamp': (True, 3, 1, 3, 100, 12),
+            'hind_inc': 0.5235987755982988
+        },
+        'TS_REF_2': {
+            'reacs': ['InChI=1S/CH3/h1H3', 'InChI=1S/H2O2/c1-2/h1-2H'],
+            'prods': ['InChI=1S/CH4/h1H4', 'InChI=1S/HO2/c1-2/h1H'],
+            'charge': 0, 'inchi': '', 'mult': 2,
+            'ts_locs': (0,),
+            'rxn_info': (
+                (('InChI=1S/CH3/h1H3', 'InChI=1S/H2O2/c1-2/h1-2H'),
+                 ('InChI=1S/CH4/h1H4', 'InChI=1S/HO2/c1-2/h1H')),
+                ((0, 0), (0, 0)), ((2, 1), (1, 2)), 2)
+        },
+        'TS_REF_3': {
+            'reacs': ['InChI=1S/C2H5/c1-2/h1H2,2H3', 'InChI=1S/H2O/h1H2'],
+            'prods': ['InChI=1S/C2H6/c1-2/h1-2H3', 'InChI=1S/HO/h1H'],
+            'charge': 0, 'inchi': '', 'mult': 2,
+            'ts_locs': (0,),
+            'rxn_info': (
+                (('InChI=1S/C2H5/c1-2/h1H2,2H3', 'InChI=1S/H2O/h1H2'),
+                 ('InChI=1S/C2H6/c1-2/h1-2H3', 'InChI=1S/HO/h1H')),
+                ((0, 0), (0, 0)), ((2, 1), (1, 2)), 2)},
+        'REF_4': {
+            'smiles': 'O',
+            'inchi': 'InChI=1S/H2O/h1H2',
+            'inchikey': 'XLYOFNOQVPJJNP-UHFFFAOYSA-N',
+            'charge': 0, 'mult': 1,
+            'mc_nsamp': (True, 3, 1, 3, 100, 12),
+            'hind_inc': 0.5235987755982988
+        },
+        'TS_REF_5': {
+            'reacs': ['InChI=1S/CH3/h1H3', 'InChI=1S/H2O/h1H2'],
+            'prods': ['InChI=1S/CH4/h1H4', 'InChI=1S/HO/h1H'],
+            'charge': 0, 'inchi': '', 'mult': 2,
+            'ts_locs': (0,),
+            'rxn_info': (
+                (('InChI=1S/CH3/h1H3', 'InChI=1S/H2O/h1H2'),
+                 ('InChI=1S/CH4/h1H4', 'InChI=1S/HO/h1H')),
+                ((0, 0), (0, 0)), ((2, 1), (1, 2)), 2)}
+        }
+
+    bas_dct = {
+        'ts_1_1_1': (
+            ['InChI=1S/CH4O/c1-2/h2H,1H3', 'InChI=1S/C2H6/c1-2/h1-2H3',
+             (('InChI=1S/CH3/h1H3', 'InChI=1S/H2O2/c1-2/h1-2H'),
+              ('InChI=1S/CH4/h1H4', 'InChI=1S/HO2/c1-2/h1H')),
+             (('InChI=1S/C2H5/c1-2/h1H2,2H3', 'InChI=1S/H2O/h1H2'),
+              ('InChI=1S/C2H6/c1-2/h1-2H3', 'InChI=1S/HO/h1H')),
+             'InChI=1S/CH4/h1H4', 'InChI=1S/H2O/h1H2',
+             (('InChI=1S/CH3/h1H3', 'InChI=1S/H2O/h1H2'),
+              ('InChI=1S/CH4/h1H4', 'InChI=1S/HO/h1H'))],
+            [1.0, 3.0, 1.0, 2.0, -3, -1, -2]
+        )
+    }
+
+    uniref_dct = thermfit.unique_basis_species(bas_dct, SPC_DCT)
+    assert ref_uni_dct == uniref_dct
 
 
 if __name__ == '__main__':
     test__species()
     test__transition_state()
+    test__unique()
