3fe4770944bd3db7ba9e7de314f7947fb119102a
snelliott <sarah.elliott26@uga.edu>
2021-08-24 16:15:13 -0500

    rrho partition functions

--- thermfit/pf.py
+++ thermfit/pf.py
@@ -5,6 +5,14 @@
 """
 
 import numpy
+import tempfile
+
+from phydat import phycon
+import automol.geom
+import autorun
+from autorun.thermp import direct as thermp_direct
+import thermp_io
+import mess_io
 
 
 def combine(pfs, coeffs, operators):
@@ -59,3 +67,221 @@ def _combine_pfs(pfa, pfb, coeff, operator):
         d2q_dt2 = [a-b-numpy.log(coeff) for a, b in zip(d2q_dt2a, d2q_dt2b)]
 
     return tempsa, tuple(logq), tuple(dq_dt), tuple(d2q_dt2)
+
+
+def q_translational(mass, temp):
+    """ Caulculate the translational partition function
+    """
+    return ((2 * numpy.pi * (mass * phycon.AMU2KG) * phycon.KB * temp)**(3/2)
+            * (phycon.H * 100.)**(-3))
+
+
+def q_rotational(i_a, i_b, i_c, sigma, temp, linear=False):
+    """ Caulculate the rotational partition function
+    """
+
+    def _A(i_x):
+        return phycon.H / (8*numpy.pi**2 * i_x)
+
+    def _q_rotational_linear(i_a, i_b, i_c, sigma, temp):
+        i_a = max(i_a, i_b, i_c)
+        i_a = i_a * phycon.AMU2KG * phycon.BOHR2CM**2 / 100**2 # amu*bohr^2 to kg m^2
+        return (phycon.KB * temp) / (phycon.H * _A(i_a) * sigma)
+
+    def _q_rotational_nonlinear(i_a, i_b, i_c, sigma, temp):
+        i_a = i_a * phycon.AMU2KG * phycon.BOHR2CM**2 / 100**2 # amu*bohr^2 to kg m^2
+        i_b = i_b * phycon.AMU2KG * phycon.BOHR2CM**2 / 100**2
+        i_c = i_c * phycon.AMU2KG * phycon.BOHR2CM**2 / 100**2
+        return ((phycon.KB * temp / phycon.H)**(3/2) * numpy.pi**(1/2)
+                * (_A(i_a) * _A(i_b) * _A(i_c))**(-1/2) / sigma)
+    
+    if linear:
+        q_rot = _q_rotational_linear(i_a, i_b, i_c, sigma, temp)
+    else:
+        q_rot = _q_rotational_nonlinear(i_a, i_b, i_c, sigma, temp)
+    return q_rot
+
+
+def q_vibrational(freqs, temp):
+    """ Caulculate the vibrational partition function
+    """
+    q_vib = 1.
+    for freq in freqs:
+    #    nu_i = freq * phycon.WAVEN2EH * phycon.EH2KJ * 1000.  # put freqs in J/mol
+        nu_i = freq * phycon.SOLMS * 100.   # put freqs in 1/s
+        # numerator = numpy.exp(-(phycon.H * nu_i) / (2 * phycon.KB * temp))
+        numerator = 1.
+        denominator = 1 - numpy.exp(-(phycon.H * nu_i) / (phycon.KB * temp))
+        q_vib *= numerator / denominator
+    return q_vib
+
+
+def rrho_partition_function(geo, freqs, temp_range=None, nlog=0):
+    """ Calculate the total rrho partition function
+    """
+    q_total = {}
+    if temp_range is None:
+        temp_range = list(range(300, 3000, 100))
+    mass = automol.geom.total_mass(geo)
+    moms = automol.geom.moments_of_inertia(geo)
+    linear = automol.geom.is_linear(geo)
+    sigma = automol.geom.external_symmetry_factor(geo)
+    for temp in temp_range:
+        q_elec = 1.
+        q_trans = q_translational(mass, temp)
+        q_rot = q_rotational(*moms, sigma, temp, linear=linear)
+        q_vib = q_vibrational(freqs, temp)
+        q_all = q_elec * q_trans * q_rot * q_vib
+        if nlog==0:
+            q_total[round(temp, 4)] = q_all
+        elif nlog==1:
+            q_total[round(temp, 4)] = numpy.log(q_all)
+        elif nlog==2:
+            q_total[round(numpy.log(temp), 4)] = numpy.log(q_all)
+    return q_total
+
+
+def pf_polys(pf_temp_dct, order=3):
+    """
+    """
+    pf_array, temp_array = list(pf_temp_dct.values()), list(pf_temp_dct.keys())
+    poly = numpy.polyfit(temp_array, pf_array, order)
+    func = numpy.poly1d(poly)
+    der = func.deriv()
+    der2 = der.deriv()
+    return func, der, der2
+
+
+def heat_capacity_from_pf(lnq, dlnqdt, d2lnqdt2, temp):
+    """
+    """
+    return (
+        phycon.NAVO * phycon.KB
+        * (temp**2 * d2lnqdt2(temp) + 2 * dlnqdt(numpy.log(temp)) + 1)
+        ) * phycon.J2CAL
+
+
+def enthalpy_from_pf(pf_fun, dqdt, temp):
+    """
+    """
+    return phycon.RC_KCAL * temp * (temp / pf_fun(temp) * dqdt(temp) + 1)
+
+
+def entropy_from_pf(pf_fun, dqdt, temp):
+    """ NOT WORKING
+    """
+    entropy = phycon.NAVO * phycon.KB * (
+        temp/pf_fun(temp)*dqdt(temp)
+        + numpy.log(pf_fun(temp))
+        - numpy.log(phycon.NAVO) + 1)
+    return entropy * phycon.J2CAL
+
+
+def internal_energy_from_pf(pf_fun, temp):
+    """
+    """
+    energy = - phycon.NAVO * phycon.KB * temp * numpy.log(pf_fun(temp))
+    return energy * phycon.J2CAL / 1000.
+
+
+#def evaluate_gibbs(geo, freqs):
+    
+    # for temp in [200, 300, 400, 500, 600, 700, 800, 900, 1000, 1500]:
+    #     temp_range = numpy.arange(temp, temp+20, .05)
+    #     heat_cap = 0
+    #     if temp > 20:
+    #         lnq_total = rrho_partition_function(geo, freqs, temp_range, nlog=1)
+    #         lnq_lnt = rrho_partition_function(geo, freqs, temp_range, nlog=2)
+    #         _, dlnqdlnt, _ = pf_polys(lnq_lnt)
+    #         lnq, dlnqdt, d2lnqdt2 = pf_polys(lnq_total)
+    #         heat_cap = heat_capacity_from_pf(lnq, dlnqdlnt, d2lnqdt2, temp)
+    #     q_total = rrho_partition_function(geo, freqs, temp_range, nlog=0)
+    #     pf_fun, dqdt, d2qdt2 = pf_polys(q_total)
+    #         heat_cap = heat_capacity_from_pf(lnq, dlnqdlnt, d2lnqdt2, temp)
+    #     entropy = entropy_from_pf(pf_fun, dqdt, temp)
+    #     enthalpy = enthalpy_from_pf(pf_fun, dqdt, temp)
+    # return entropy
+
+
+# def gibbs_from_property_dct(csh_t_dct, hform0k):
+#     """
+#     """
+#     gibbs_t_dct = {}
+#     for temp in csh_t_dct:
+#         _, entropy, enthalpy = csh_t_dct[temp]
+
+def fake_mess_rrho_partition_function(geo, freqs, hform0, temps):
+    """
+    """
+    def _pf_arrays(geo, freqs, temps):
+        temps_tuple = ()
+        lnq_tuple = ()
+        dlnqdt_tuple = ()
+        d2lnqdt2_tuple = ()
+        #####
+        q_tuple = ()
+        dqdt_tuple = ()
+        d2qdt2_tuple = ()
+        dlnqdlnt_tuple = ()
+        entropy = ()
+        enthalpy = ()
+        heat_cap = ()
+        for temp in temps:
+            temp_range = numpy.arange(temp-20, temp+50, .01)
+            lnq_temp_dct = rrho_partition_function(
+                geo, freqs, temp_range, nlog=1)
+            lnq_func, dlnqdt_func, d2lnqdt2_func = pf_polys(
+                lnq_temp_dct, order=3)
+            temps_tuple += (temp,)
+            lnq_tuple += (lnq_func(temp),)
+            dlnqdt_tuple += (dlnqdt_func(temp),)
+            d2lnqdt2_tuple += (d2lnqdt2_func(temp),)
+            #####
+            q_temp_dct = rrho_partition_function(
+                geo, freqs, temp_range, nlog=0)
+            lnq_lnt_dct = rrho_partition_function(
+                geo, freqs, temp_range, nlog=2)
+            q_func, dqdt_func, d2qdt2_func = pf_polys(
+                q_temp_dct, order=3)
+            _, dlnqdlnt, _ = pf_polys(
+                lnq_lnt_dct, order=2)
+            q_tuple += (q_func(temp),)
+            dqdt_tuple += (dqdt_func(temp),)
+            d2qdt2_tuple += (d2qdt2_func(temp),)
+            d2lnqdlnt2_tuple += (d2lnqdlnt2_func(temp),)
+
+            heat_cap_i = heat_capacity_from_pf(lnq_func, dlnqdlnt_func, d2lnqdt2_func, temp)
+            entropy_i = entropy_from_pf(q_func, dqdt_func, temp)
+            enthalpy_i = enthalpy_from_pf(q_func, dqdt_func, temp)
+            heat_cap += (heat_cap_i,)
+            entropy += (entropy_i,)
+            enthalpy += (enthalpy_i,)
+        print('Partition Function')
+        for temp, y_val, dydx, d2ydx2 in zip(temps_tuple, q_tuple, dqdt_tuple, d2qdt2_tuple):
+            print(temp, y_val, dydx, d2ydx2)
+        print('LOG Partition Function')
+        for temp, y_val, dydx, d2ydx2 in zip(temps_tuple, lnq_tuple, dlnqdt_tuple, d2lnqdt2_tuple):
+            print(temp, y_val, dydx, d2ydx2)
+        print('Temp', 'Heat Cap', 'Entropy', 'Enthalpy')
+        for temp, heat_cap_i, entropy_i, enthalpy_i in zip(temps_tuple, entropy, enthalpy, heat_cap):
+            print(temp, heat_cap_i, entropy_i, enthalpy_i)
+        return temps_tuple, lnq_tuple, dlnqdt_tuple, d2lnqdt2_tuple
+
+    formula_str = automol.geom.formula_string(geo)
+    temps.append(298.15)
+    pf_arrays = _pf_arrays(geo, freqs, temps)
+    pf_str = mess_io.writer.pf_output(formula_str, *pf_arrays)
+    rundir = 'tmp'
+    print(pf_arrays)
+    #with tempfile.TemporaryDirectory() as rundir:
+    thermp_script_str = autorun.SCRIPT_DCT['thermp']
+    _, thermp_output_strs = thermp_direct(
+        thermp_script_str, rundir,
+        pf_str, formula_str, hform0, temps[:-1])
+    print(thermp_output_strs[0])
+    print(temps[:-1])
+    csh_t_dct = thermp_io.reader.properties_temp_dct(thermp_output_strs[0])
+    print(csh_t_dct)
+
+    
+    # write_mess_output(formula_string, pf_arrays, rundir)
