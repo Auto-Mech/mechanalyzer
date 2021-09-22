ff1dc52b637136577ed4b1d6de868d2c80c96198
snelliott <sarah.elliott26@uga.edu>
2021-09-22 15:03:26 -0500

    see load therm function

--- mechanalyzer/parser/__init__.py
+++ mechanalyzer/parser/__init__.py
@@ -6,11 +6,12 @@ from mechanalyzer.parser import pes
 from mechanalyzer.parser import spc
 from mechanalyzer.parser import mech
 from mechanalyzer.parser._bld import build_input_file
-
+from mechanalyzer.parser.ckin_ import load_spc_therm_dct
 
 __all__ = [
     'pes',
     'spc',
     'mech',
-    'build_input_file'
+    'build_input_file',
+    'load_spc_therm_dct'
 ]
