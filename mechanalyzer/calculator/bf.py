960432325915864c1dacf26bc192b50a89abf3b5
Luna Pratali Maffei <lmaffei@beboplogin2.lcrc.anl.gov>
2022-09-06 17:03:18 -0500

    small bug fix

--- mechanalyzer/calculator/bf.py
+++ mechanalyzer/calculator/bf.py
@@ -228,10 +228,11 @@ def bf_tp_df_todct(bf_tp_df, bf_threshold, savefile=False, rxn='', model=''):
         # write file with the BFs
         if num_data_highenough > 0 and savefile:
             bf_df_sp_i = bf_df_sp_i.reset_index()
+            bf_df_sp_i = bf_df_sp_i.rename(columns = {'index': 0})
             header_label = np.array(sorted(bf_df_sp_i.columns[1:]), dtype=str)
-            header_label[0] = 'T [K]'
+            header_label = np.insert(header_label, 0, 'T [K]')
             labels = '\t\t'.join(header_label)
-            np.savetxt(f'bf_{model}_{rxn}_{spc}.txt', bf_df_sp_i[sorted(bf_df_sp_i.columns[1:])].values,
+            np.savetxt(f'bf_{model}_{rxn}_{spc}.txt', bf_df_sp_i[sorted(bf_df_sp_i.columns)].values,
                        delimiter='\t', header=labels, fmt='%1.2e')
 
     return bf_tp_dct_out
