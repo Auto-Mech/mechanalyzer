b2e5502b8ecc97756c1fd474a7aa85c979d24123
snelliott <sarah.elliott26@uga.edu>
2023-04-25 15:57:07 -0500

    increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name="mechanalyzer",
-    version="0.7.0",
+    version="0.8.0",
     packages=[
         'mechanalyzer',
         'mechanalyzer.builder',
