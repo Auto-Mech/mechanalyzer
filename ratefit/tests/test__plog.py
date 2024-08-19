99bf2835f06d6adbe70bb0480c0a6f85c2e2f3be
Luna Pratali Maffei <luna.pratali@polimi.it>
2024-08-19 19:43:42 +0200

    added plog test specifically for dup

--- ratefit/tests/test__plog.py
+++ ratefit/tests/test__plog.py
@@ -33,6 +33,28 @@ KTP_DCT = {
     10.0:  (TEMPS, KTS4),
     100.0: (TEMPS, KTS5)}
 
+# MORE COMPLEX FITS; CHECK FIT ACCURACY WITH SCIPY.OPTIMIZE
+KTP_DCT_003 = {0.03: (numpy.array([ 700,  800,  900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700,
+       1800, 1900, 2000]), numpy.array([1.70839926e+01, 3.59052809e+02, 3.29199792e+03, 1.65649708e+04,
+       5.25326997e+04, 1.16776837e+05, 1.97924251e+05, 2.73040784e+05,
+       3.22394912e+05, 3.38361666e+05, 3.23964108e+05, 2.87644090e+05,
+       2.39691713e+05, 1.89607419e+05])), }
+KTP_DCT_01 = {0.1: (numpy.array([ 700,  800,  900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700,
+       1800, 1900, 2000]), numpy.array([1.17042854e+01, 3.62340701e+02, 3.96695222e+03, 2.17418827e+04,
+       7.28404669e+04, 1.71444252e+05, 3.12533539e+05, 4.71391229e+05,
+       6.15833072e+05, 7.19922497e+05, 7.70306454e+05, 7.65803355e+05,
+       7.15134504e+05, 6.33489714e+05])), }
+KTP_DCT_1 = {1.0: (numpy.array([ 700,  800,  900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700,
+       1800, 1900, 2000]), numpy.array([6.09370479e-01, 4.82578824e+01, 1.05773202e+03, 9.79865455e+03,
+       5.00060751e+04, 1.67098531e+05, 4.10455552e+05, 8.03791141e+05,
+       1.33361425e+06, 1.96485747e+06, 2.65818345e+06, 3.36101673e+06,
+       3.99605414e+06, 4.48473231e+06])),}
+KTP_DCT_10 = {10.0: (numpy.array([ 700,  800,  900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700,
+       1800, 1900, 2000]), numpy.array([4.26692513e-01, 1.93815729e+01, 3.37509413e+02, 3.05107943e+03,
+       1.75693053e+04, 7.73320057e+04, 3.03780241e+05, 1.02326408e+06,
+       2.56408368e+06, 4.67402312e+06, 6.80145399e+06, 8.93009298e+06,
+       1.15236982e+07, 1.50188781e+07]))}
+
 
 def test_plog():
 
@@ -40,6 +62,23 @@ def test_plog():
     max_err = err.get_max_err(err_dct)
     assert max_err < 25  # error in %
 
+def test_plog_dbltols():
+
+    params, err_dct = plog.get_params(KTP_DCT_003, dbltol=1)
+    max_err = err.get_max_err(err_dct)
+    assert max_err < 0.6  # error in %
+    params, err_dct = plog.get_params(KTP_DCT_01, dbltol=0.1)
+    max_err = err.get_max_err(err_dct)
+    assert max_err < 0.3  # error in %
+    params, err_dct = plog.get_params(KTP_DCT_1, dbltol=0.1)
+    max_err = err.get_max_err(err_dct)
+    assert max_err < 0.5  # error in %
+    params, err_dct = plog.get_params(KTP_DCT_10, dbltol=1)
+    max_err = err.get_max_err(err_dct)
+    assert max_err < 11.5  # error in %
+
 
 if __name__ == '__main__':
     test_plog()
+    test_plog_dbltols()
+
